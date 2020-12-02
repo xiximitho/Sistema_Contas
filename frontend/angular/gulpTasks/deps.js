ee0740d162f3ad1c2759eca62b83fcbd75012138
Felipe Mattes <mattesfelipe@gmail.com>
2020-12-02 20:31:48 -0300

    Criado a build do frontend.

---
+++ frontend/angular/gulpTasks/deps.js
@@ -0,0 +1,45 @@
+const gulp = require('gulp')
+const uglify = require('gulp-uglify')
+const uglifycss = require('gulp-uglifycss')
+const concat = require('gulp-concat')
+const htmlmin = require('gulp-htmlmin')
+
+
+gulp.task('deps.js', () => {
+    return gulp.src(
+        'node_modules/angular/angular.min.js',
+        'node_modules/angular-ui-router/release/angular-ui-router.min.js',
+        'node_modules/angular-animate/angular-animate.min.js',
+        'node_modules/angular-toastr/dist/angular-toastr.min.js',
+        'node_modules/admin-lte/plugins/jQuery/jquery.min.js',
+        'node_modules/admin-lte/bootstrap/js/bootstrap.min.js',
+        'node_modules/admin-lte/plugins/slimScroll/jquery.slimScroll.min.js',
+        'node_modules/admin-lte/dist/js/app.min.js',
+    )
+    .pipe(uglify()) //deixa o arquivo compactado
+    .pipe(concat('deps.min.js'))
+    .pipe(gulp.dest('public/assets/js')) //Reduz o arquivo e manda para o path public/assets/js
+})
+
+gulp.task('deps.css', () => {
+    return gulp.src([
+        'node_modules/angular-toastr/dist/angular-toastr.min.css',
+        'node_modules/font-awesome/css/font-awesome.min.css',
+        'node_modules/admin-lte/bootstrap/css/bootstrap.min.css',
+        'node_modules/admin-lte/dist/css/AdminLTE.min.css',
+        'node_modules/admin-lte/dist/css/skins/_all-skins.min.css'
+    ])
+    .pipe(uglifycss({"uglyComments": true}))
+    .pipe(concat('deps.min.css'))
+    .pipe(gulp.dest('public/assets/css')) //Reduz o arquivo e manda para o path public/assets/css
+})
+
+gulp.task('deps.fonts', () => {
+    return gulp.src([
+        'node_modules/font-awesome/fonts/*.*',
+        'node_modules/admin-lte/bootstrap/fonts/*.*'
+    ])
+    .pipe(gulp.dest('public/assets/fonts'))
+})
+
+gulp.task('deps', gulp.series('deps.js','deps.css','deps.fonts')) //'deps.fonts'
